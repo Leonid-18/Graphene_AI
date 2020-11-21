46b746aa322ce69015edc4b2a5f775724f52ae5f
Леонид Ткаченко <leonidtkacenko@MacBook-Pro-Leonid.local>
2020-11-22 00:39:19 +0100

    *add erode && correcting brightness && histogram

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ import numpy as np
 import cv2
 import glob
 from PIL import Image
+from matplotlib import pyplot as plt
 
 class AnalysePhoto:
     
@@ -18,7 +19,6 @@ class AnalysePhoto:
         
     def __read__(self, image):
         for img in glob.glob(self.path):
-            print('PATH')
             image = cv2.imread(img)
         return image
     
@@ -34,16 +34,36 @@ class AnalysePhoto:
             #image_array.append(image_item)
         return image_item
     
+    def __addBrightness__(self, image):
+        
+        # Converting image to LAB Color model
+        lab= cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+        
+        # Splitting the LAB image to different channels
+        l, a, b = cv2.split(lab)
+        
+        # Applying CLAHE to L-channel
+        clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(8,8))
+        cl = clahe.apply(l)
+        
+        # Merge the CLAHE enhanced L-channel with the a and b channel
+        limg = cv2.merge((cl,a,b))
+        final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+        return final
+        
+    
+    def __erode__(self, image, erode):
+        element = np.ones((erode,erode), 'uint8')
+        return cv2.erode(image, element, iterations=1)
+    
     def __gaussianBlur__(self, image):
-        get_image = self.__resize__(image)
-        gray = cv2.cvtColor(get_image,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         kernel_size = 5
         blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
         return blur_gray
     
-    def __cannyEdge__(self, gray):
-        low_threshold = 50
-        high_threshold = 150
+    
+    def __cannyEdge__(self, gray, low_threshold, high_threshold):
         edges = cv2.Canny(gray, low_threshold, high_threshold)
         return edges
     
@@ -77,26 +97,77 @@ class AnalysePhoto:
         
         
 
-image1 = AnalysePhoto('./Data/G038_002.tif')
-image2 = AnalysePhoto('./Data/G038_002.tif')
+image = AnalysePhoto('./Data/G038_002.tif')
 
-gaussian = image1.__gaussianBlur__(image1)
+image_first = image.__resize__(image)
 
-edges = image1.__cannyEdge__(gaussian)
+#-----Gaussian-------------------------------------------
+gaussian = image.__gaussianBlur__(image_first)
 
-auto_edges = image2.__autoCanny__(gaussian, 0.3)
+#-----Manual Cany -------------------------------------------
+edges = image.__cannyEdge__(gaussian, 50, 150)
+lines_edges = image.__HoughLinesP__(image_first, edges)
 
-lines_edges = image1.__HoughLinesP__(image1.__resize__(image1), edges)
+#-----Auto Cany-------------------------------------------
 
-lines_auto_edges = image2.__HoughLinesP__(image2.__resize__(image1), auto_edges)
+auto_edges = image.__autoCanny__(gaussian, 0.3)
+lines_auto_edges = image.__HoughLinesP__(image_first, auto_edges)
+
+#-----Gaussian with Erode-------------------------------------------
+
+gaussina_erode = image.__erode__(gaussian, 2)
+
+#-----Auto Cany with Erode-------------------------------------------
+
+auto_edges_erode = image.__autoCanny__(gaussina_erode, 0.3)
+lines_auto_edges_erode = image.__HoughLinesP__(image_first, auto_edges_erode)
+
+#-----Manual Cany with Erode -------------------------------------------
+
+manual_edges_erode = image.__cannyEdge__(gaussina_erode, 50, 150)
+lines_edges_manual = image.__HoughLinesP__(image_first, manual_edges_erode)
+
+
+#-----Gaussian with Erode and correcting brightness----------------------------
+
+
+addBrightness = image.__addBrightness__(image_first)
+
+gaussian_brightness = image.__gaussianBlur__(addBrightness)
+
+gaussina_brightness_erode = image.__erode__(gaussian_brightness, 3)
+
+manual_edges_erode_brightness = image.__cannyEdge__(gaussina_brightness_erode, 200, 300)
+lines_edges_manual_brightness = image.__HoughLinesP__(image_first, manual_edges_erode_brightness)
 
 
 
+
+
+
+
+cv2.imshow('Erode', gaussina_erode)
 cv2.imshow("Gaussian image", gaussian)
 cv2.imshow("Canny image", edges)
-cv2.imshow("Auto Canny image", auto_edges)
+
+#cv2.imshow("Auto Canny image", auto_edges)
 cv2.imshow('HoughLinesP', lines_edges)
-cv2.imshow('Auto HoughLinesP', lines_auto_edges)
+#cv2.imshow('Auto HoughLinesP', lines_auto_edges)
+
+cv2.imshow('HoughLinesP with Erode', lines_edges_manual)
+#cv2.imshow('Auto HoughLinesP with Erode', lines_auto_edges_erode)
+cv2.imshow('correcting brightness', addBrightness)
+cv2.imshow('HoughLinesP with Erode and correcting brightness', lines_edges_manual_brightness)
+
+hist,bins = np.histogram(lines_edges_manual.flatten(),256,[0,256])
+cdf = hist.cumsum()
+cdf_normalized = cdf * float(hist.max()) / cdf.max()
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(lines_edges_manual.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
